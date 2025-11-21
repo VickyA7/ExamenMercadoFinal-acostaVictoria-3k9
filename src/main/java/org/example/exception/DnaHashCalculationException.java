97bcd9ce73b2d8ee614f99b70eaf0e0e4fda4e45
VickyA7 <vico.ac2507@gmail.com>
2025-11-21 18:37:17 -0300

    Clase DnaHashCalculationException
    Aca hacemos una clase para que maneje las excepciones de manera mas personalizada. Es decir avisos de errores más especificos

--- src/main/java/org/example/exception/DnaHashCalculationException.java
+++ src/main/java/org/example/exception/DnaHashCalculationException.java
@@ -1,4 +1,11 @@
-package java.org.example.exception;
+package org.example.exception;
 
-public class DnaHashCalculationException {
+// Creamos nuestra propia clase de excepción extendiendo RuntimeException.
+
+// Esto nos permite ser más específicos sobre qué tipo de error ocurrió.
+public class DnaHashCalculationException extends RuntimeException {
+
+    public DnaHashCalculationException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
